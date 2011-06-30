ac06aaa1a49761f2f745f7231ffa82f4cfadb088
arisbartee <arisbartee@gmail.com>
2011-06-30 07:02:13 -0500

    iweb site
    My iweb created site

---
+++ Howdy_files/Howdy.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.3 local-build-20110630
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Howdy_files/Creme_frame2_01.png'),IWCreateImage('Howdy_files/Creme_frame2_02.png'),IWCreateImage('Howdy_files/Creme_frame2_04.png'),IWCreateImage('Howdy_files/Creme_frame2_08.png'),IWCreateImage('Howdy_files/Creme_frame2_09.png'),IWCreateImage('Howdy_files/Creme_frame2_14.png'),IWCreateImage('Howdy_files/Creme_frame2_13.png'),IWCreateImage('Howdy_files/Creme_frame2_05.png')],null,0,1.000000,3.000000,3.000000,3.000000,3.000000,23.000000,23.000000,23.000000,23.000000,20.000000,19.000000,20.000000,19.000000,null,null,null,0.400000),stroke_1:new IWPhotoFrame([IWCreateImage('Howdy_files/Creme_sidebar_frame_01.png'),IWCreateImage('Howdy_files/Creme_sidebar_frame_02.png'),IWCreateImage('Howdy_files/Creme_sidebar_frame_03.png'),IWCreateImage('Howdy_files/Creme_sidebar_frame_06.png'),IWCreateImage('Howdy_files/Creme_sidebar_frame_09.png'),IWCreateImage('Howdy_files/Creme_sidebar_frame_08.png'),IWCreateImage('Howdy_files/Creme_sidebar_frame_07.png'),IWCreateImage('Howdy_files/Creme_sidebar_frame_04.png')],null,2,1.000000,0.000000,0.000000,0.000000,0.000000,10.000000,16.000000,10.000000,20.000000,523.000000,173.000000,523.000000,173.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Howdy_files/HowdyMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();fixupIECSS3Opacity('id9');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ Resume_files/Resume.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20110630
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Resume_files/ResumeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
